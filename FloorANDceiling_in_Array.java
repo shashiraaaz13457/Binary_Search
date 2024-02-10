e7d457b6af5c856fdb88e28c7db7a4bf62c191bb
Shashi raj <shashiraj13457@gmail.com>
2024-02-10 15:08:13 +0530

    Floor and Ceiling

---
+++ FloorANDceiling_in_Array.java
@@ -0,0 +1,40 @@
+public class FloorANDceiling_in_Array {
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,9,14,16,18};
+        int target = 15;
+//        int ans = ceiling(arr,target);
+        int ans = floor(arr,target);
+        System.out.println(ans);
+    }
+    //Ceiling of a number
+//    static int ceiling(int[] arr,int target){
+//        int start = 0;
+//        int end = arr.length-1;
+//        while (start<=end){
+//            int mid = start + (end-start);
+//            if(target<arr[mid]){
+//                end = mid - 1 ;
+//            } else if (target>arr[mid]) {
+//                start = mid + 1;
+//            }else {
+//                return arr[mid];
+//            }
+//        }
+//        return arr[start];
+//    }
+    static int floor(int[] arr,int target){
+        int start = 0;
+        int end = arr.length-1;
+        while (start<=end){
+            int mid = start + (end-start);
+            if(target<arr[mid]){
+                end = mid - 1 ;
+            } else if (target>arr[mid]) {
+                start = mid + 1;
+            }else {
+                return arr[mid];
+            }
+        }
+        return arr[end];
+    }
+}
