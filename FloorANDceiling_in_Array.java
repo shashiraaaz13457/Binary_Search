4500173b342c43caef82ca1cab9aead488b5a318
Shashi raj <shashiraj13457@gmail.com>
2024-03-02 20:19:07 +0530

    Infinite Array

--- FloorANDceiling_in_Array.java
+++ FloorANDceiling_in_Array.java
@@ -1,7 +1,7 @@
 public class FloorANDceiling_in_Array {
     public static void main(String[] args) {
         int[] arr = {2,3,5,9,14,16,18};
-        int target = 15;
+        int target = 15 ;
 //        int ans = ceiling(arr,target);
         int ans = floor(arr,target);
         System.out.println(ans);
